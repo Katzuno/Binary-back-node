6d355cbe1782d829cb61f9faba1218eb0e41fa95
Statescu Razvan <razvan_statescu@yahoo.com>
2019-11-17 01:52:32 +0200

    fix: send image to server

--- src/helpers/WebSocketsHelper.js
+++ src/helpers/WebSocketsHelper.js
@@ -3,7 +3,7 @@ const axios = require('axios');
 
 const onMessage = async (socket, msg) => {
     const requestData = {
-        base64img: msg.split(',')[1],
+        base64img: msg.image.split(',')[1],
         height: msg.height
     }
     const res = (await axios.post('http://46.101.195.188:5000/api/analyse', requestData)).data;
