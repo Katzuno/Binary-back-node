b7620cf4fedcaf430b5fa11e5dad723758893bd3
Statescu Razvan <razvan_statescu@yahoo.com>
2019-11-17 01:41:22 +0200

    send heigth to erik

--- src/helpers/WebSocketsHelper.js
+++ src/helpers/WebSocketsHelper.js
@@ -2,13 +2,14 @@ const Nes = require('@hapi/nes');
 const axios = require('axios');
 
 const onMessage = async (socket, msg) => {
-    // console.log(msg);
-    // await socket.server.publish('/room/' + msg.id, msg);
-    // console.log('img', msg);
-    const res = (await axios.post('http://46.101.195.188:5000/api/analyse', {base64img: msg.split(',')[1]})).data;
-    // console.log(res)
+    const requestData = {
+        base64img: msg.split(',')[1],
+        height: msg.height
+    }
+    const res = (await axios.post('http://46.101.195.188:5000/api/analyse', requestData)).data;
+
     await socket.send({type: 'imageData', data: res});
-    // console.log('sent msg')
+
     return true;
 };
 
