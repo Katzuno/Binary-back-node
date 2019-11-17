fe25dd2f5e75e18d9a265e7d837edfcbccf40313
Statescu Razvan <razvan_statescu@yahoo.com>
2019-11-17 08:34:26 +0200

    face recognition

--- src/helpers/WebSocketsHelper.js
+++ src/helpers/WebSocketsHelper.js
@@ -1,6 +1,12 @@
 const Nes = require('@hapi/nes');
 const axios = require('axios');
 
+const checkFace = img => {
+    return axios.post('http://46.101.195.188:5000/api/face', {img_url: img}).then(res => {
+        return res.data;
+    });
+}
+
 const onMessage = async (socket, msg) => {
     const requestData = {
         base64img: msg.image.split(',')[1],
@@ -8,6 +14,10 @@ const onMessage = async (socket, msg) => {
         width: msg.width
     }
     const res = (await axios.post('http://46.101.195.188:5000/api/analyse', requestData)).data;
+
+    const resFace = await checkFace(requestData.base64img);
+    if (!resFace.error)
+        await socket.send({type: 'face', data: resFace});
 
     await socket.send({type: 'imageData', data: res});
 
